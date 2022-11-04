30aa59fbeb6de9e25589e6b9dae852618b98b294
Hanna Arefiardakani <darkshiirarhotmail.com>
2022-11-05 00:14:17 +0100

    added a form for the poll

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,19 @@
 let date = document.querySelector("#date");
+let voting = document.querySelector("#votebutton");
+let votingform = document.querySelector("#votingform");
+let remove = document.querySelector("#remove");
 
 let today = new Date();
+
+voting.addEventListener("click", e =>{
+    e.preventDefault();
+    votingform.style.display = "block";
+});
+
+remove.addEventListener("click", e =>{
+    e.preventDefault();
+    votingform.style.display = "none";
+});
 
 
 let li = document.createElement("li");
